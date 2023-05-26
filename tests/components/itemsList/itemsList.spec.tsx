d7301dcddc070f9f00bb055655b7a2f50101d106
mrcs <marcusmoraes2010@hotmail.com>
2023-05-25 22:11:39 -0300

    feat: test <itemsList/> for rendering title and subtitle

--- tests/components/itemsList/itemsList.spec.tsx
+++ tests/components/itemsList/itemsList.spec.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ItemsList from '../../../src/componets/itemsList/ItemsList';
-import { createItems } from './itemsFactory';
+import * as itemsFactory from './itemsFactory';
 
 
 describe('<ItemsList />', () => {
   it('should render all items', () => {
-    const items = createItems();
+    const items = itemsFactory.createItems();
     render(<ItemsList items={items}/>);
 
     const renderedComponents = screen.queryAllByTestId(/child-component-\d+/);
     expect(renderedComponents).toHaveLength((items.length));
   }); 
+
+  it('should render the title when provided', () => {
+    const title = 'A beautiful title';
+    const items = itemsFactory.createItems();
+    render(<ItemsList title={title} items={items} />);
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+  });
+
+  it('should render the subtitle when provided', () => {
+    const subTitle = 'A beautiful subtitle';
+    const items = itemsFactory.createItems();
+    render(<ItemsList subTitle={subTitle} items={items} />);
+
+    expect(screen.getByText(subTitle)).toBeInTheDocument();
+  });
 });
