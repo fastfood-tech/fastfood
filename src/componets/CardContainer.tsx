3eac0b45d43da9c2bc3dff321156a7acdd0b2090
mrcs <marcusmoraes2010@hotmail.com>
2023-05-26 10:17:52 -0300

    feat: create card container

---
+++ src/componets/CardContainer.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  width: 19rem;
+  height: 13rem;
+
+  -webkit-box-shadow: 0px 5px 20px 5px rgba(244, 244, 244, 1);
+  -moz-box-shadow: 0px 5px 20px 5px rgba(244, 244, 244, 1);
+  box-shadow: 0px 5px 20px 5px rgba(244, 244, 244, 1);
+
+  background-color: #fff !important;
+
+  border-radius: 20px;
+`;
+
+export default CardContainer;
